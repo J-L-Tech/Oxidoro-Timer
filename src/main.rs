9feda0421ba3afd3471a2365c518d47503e04b5a
J-L-Tech <jorenlarm+github@gmail.com>
2024-06-14 11:12:43 -0600

    Initial Project

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+slint::include_modules!();
+
+#[cfg_attr(target_arch = "wasm32",
+           wasm_bindgen::prelude::wasm_bindgen(start))]
+pub fn main() /* -> Result<(), slint::PlatformError>*/ {
+    let ui = AppWindow::new().unwrap();
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run().unwrap()
+}
